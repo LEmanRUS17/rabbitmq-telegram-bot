9d23c9fe95478bd21e82643165d0ae45da3e7021
LEmanRUS17 <lemanrus17@gmail.com>
2026-09-18 21:03:27 +0500

    feat: add queue

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,24 +7,27 @@ import (
 
 // settings bot
 type Config struct {
-	RabbitMQURL   string
-	QueueName     string
-	TelegramToken string
+	RabbitMQURL      string
+	QueueName        string
+	UpdatesQueueName string
+	TelegramToken    string
 }
 
 func Load() (*Config, error) {
 	rabbitURL := os.Getenv("RABBITMQ_URL")
 	token := os.Getenv("TELEGRAM_TOKEN")
 	queueName := os.Getenv("QUEUE_NAME")
+	updatesQueueName := os.Getenv("UPDATES_QUEUE_NAME")
 
 	if token == "" {
 		return nil, errors.New("TELEGRAM_TOKEN not found")
 	}
 
 	cfg := &Config{
-		RabbitMQURL:   rabbitURL,
-		QueueName:     queueName,
-		TelegramToken: token,
+		RabbitMQURL:      rabbitURL,
+		QueueName:        queueName,
+		UpdatesQueueName: updatesQueueName,
+		TelegramToken:    token,
 	}
 
 	return cfg, nil
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -15,23 +15,28 @@ func TestLoad_MissingToken(t *testing.T) {
 func TestLoad_Success(t *testing.T) {
 	t.Setenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/")
 	t.Setenv("QUEUE_NAME", "test_queue")
+	t.Setenv("UPDATES_QUEUE_NAME", "test_updates_queue")
 	t.Setenv("TELEGRAM_TOKEN", "fake-token")
 
 	cfg, err := Load()
 
 	if err != nil {
-	        t.Fatalf("expected no error, got: %v", err)
+	    t.Fatalf("expected no error, got: %v", err)
 	}
 
 	if cfg.RabbitMQURL != "amqp://guest:guest@localhost:5672/" {
-	        t.Errorf("RabbitMQURL = %q, want %q", cfg.RabbitMQURL, "amqp://guest:guest@localhost:5672/")
+	    t.Errorf("RabbitMQURL = %q, want %q", cfg.RabbitMQURL, "amqp://guest:guest@localhost:5672/")
 	}
 
 	if cfg.QueueName != "test_queue" {
-	        t.Errorf("QueueName = %q, want %q", cfg.QueueName, "test_queue")
+	    t.Errorf("QueueName = %q, want %q", cfg.QueueName, "test_queue")
+	}
+
+	if cfg.UpdatesQueueName != "test_updates_queue" {
+		t.Errorf("UpdatesQueueName = %q, want %q", cfg.UpdatesQueueName, "test_updates_queue")
 	}
 
 	if cfg.TelegramToken != "fake-token" {
-	        t.Errorf("TelegramToken = %q, want %q", cfg.TelegramToken, "fake-token")
+	    t.Errorf("TelegramToken = %q, want %q", cfg.TelegramToken, "fake-token")
 	}
 }
